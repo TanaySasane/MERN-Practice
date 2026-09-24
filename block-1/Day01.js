f7ecc6eb6e206509c8ed8954ff5988db0398f360
TanaySasane <tanaysasane@gmail.com>
2026-09-24 18:31:54 +0530

    Practising JS from basics

---
+++ block-1/Day01.js
@@ -0,0 +1,11 @@
+const userName="Tanay";
+let currentDate=24;
+let isAvailable=true;
+let emptySlot=null;
+let notDefined;
+
+console.log(typeof userName);
+console.log(typeof currentDate);
+console.log(typeof isAvailable);
+console.log(typeof emptySlot);
+console.log(typeof notDefined);
---
+++ block-1/practice01.js
@@ -0,0 +1,18 @@
+// 1.Variable Typing: Declare a variable laptopBrand with the value 
+// "Lenovo", and a variable ramSize with the value 16.
+//  Print their types using typeof.
+const laptopBrand="Lenovo";
+ramSize=16;
+console.log(typeof laptopBrand);
+console.log(typeof ramSize);
+// 2.Prediction Task: What will print in the console? Write your guess, then run it with Node:
+// JavaScript
+// let city;
+// console.log(city);  o/p will be undefined 
+// city = null;
+// console.log(city);   o/p will be null
+
+// 3.Strict Check: Predict the boolean output (true or false) for both lines:
+// JavaScript
+// console.log(null == undefined);  o/p true
+// console.log(null === undefined); o/p false
